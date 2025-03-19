f0d68ad45f1b2c1e1f224358b658873edfaf4c7b
xtsvirko <alex.panzhar1@gmail.com>
2025-03-19 22:12:03 +0100

    feat: add database utilites

---
+++ src/database/repository.py
@@ -0,0 +1,8 @@
+from database.connection import Database
+from database.queries import FETCH_FACILITY_DATA
+
+
+class FacilityRepository:
+    @staticmethod
+    async def get_facilities(limit: int, offset: int):
+        return await Database.fetch(FETCH_FACILITY_DATA, limit, offset)
