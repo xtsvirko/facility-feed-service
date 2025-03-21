64783b1e19c3e65834933b6d5d05ef0d713684b4
xtsvirko <alex.panzhar1@gmail.com>
2025-03-21 10:02:40 +0100

    test: update test_metadata_generator

--- tests/test_metadata_generator.py
+++ tests/test_metadata_generator.py
@@ -4,5 +4,4 @@ from services.metadata_generate import MetadataGenerator
 def test_mock_metadata_generator():
     metadata = MetadataGenerator.generate_metadata(["mock_feed.json.gz"])
     assert "generation_timestamp" in metadata
-    assert metadata["name"] == "reservewithgoogle.entity"
     assert "mock_feed.json.gz" in metadata["data_file"]
