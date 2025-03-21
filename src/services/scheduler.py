b9757dc5c32efad753d31de84533534493dba11c
xtsvirko <alex.panzhar1@gmail.com>
2025-03-21 07:41:25 +0100

    style: refactor code style

--- src/services/scheduler.py
+++ src/services/scheduler.py
@@ -8,6 +8,8 @@ from database.connection import Database
 from services.feed_generate import FeedGenerator
 from services.metadata_generate import MetadataGenerator
 from services.s3_uploader import S3Uploader
+from utils.config import Config, config
+from utils.logger import logger
 
 
 def get_feed_filename():
@@ -16,30 +18,36 @@ def get_feed_filename():
 
 
 async def scheduled_task():
-    print("Connecting to the database...")
-    await Database.connect()  # Подключаемся к БД
+    await Database.connect()
 
-    print("Generating feed data...")
     feeds = await FeedGenerator.generate_feed()
     feed_files = []
 
     for index, feed in enumerate(feeds):
         file_name = get_feed_filename()
-        file_path = os.path.join("/tmp", file_name)
-        with gzip.open(file_path, "wt", encoding="utf-8") as f:
-            json.dump(feed, f)
+        file_path = os.path.join(Config.TMP_DIR, file_name)
+        try:
+            with gzip.open(file_path, "wt", encoding="utf-8") as f:
+                json.dump(feed, f)
+        except OSError as e:
+            logger.error(f"Writing files error {file_path}: {e}")
+            continue
 
-        # await S3Uploader.upload_to_s3(file_path, file_name)
+        await S3Uploader.upload_to_s3(file_path, file_name)
         feed_files.append(file_name)
 
-    metadata = MetadataGenerator.generate_metadata(feed_files)
-    metadata_file = os.path.join("/tmp", "metadata.json")
-    with open(metadata_file, "w", encoding="utf-8") as f:
-        json.dump(metadata, f)
+    if feed_files:
+        metadata = MetadataGenerator.generate_metadata(feed_files)
+        metadata_file = os.path.join(config.TMP_DIR, "metadata.json")
+        try:
+            with open(metadata_file, "w", encoding="utf-8") as f:
+                json.dump(metadata, f)
+            await S3Uploader.upload_to_s3(metadata_file, "metadata.json")
+        except OSError as e:
+            logger.error(f"Writing metadata files error: {e}")
+    else:
+        logger.warning("No generated feeds, metadata.json was not created")
 
-    # await S3Uploader.upload_to_s3(metadata_file, "metadata.json")
-
-    print("Disconnecting from database...")
     await Database.disconnect()
 
 
