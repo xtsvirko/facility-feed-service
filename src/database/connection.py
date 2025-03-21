177458c2afbd2d8cc5a879f6fe727e736eb08fe8
xtsvirko <oleksandr.tsvirko@gmail.com>
2025-03-21 07:41:25 +0100

    style: refactor code style

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import asyncpg
 from utils.config import config
 from utils.logger import logger
@@ -7,18 +9,21 @@ class Database:
     _pool = None
 
     @classmethod
-    async def connect(cls):
-        if cls._pool is None:
+    async def connect(cls, retries: int = 3, delay: int = 5):
+        for attempt in range(retries):
             try:
                 cls._pool = await asyncpg.create_pool(
                     dsn=config.DATABASE_URL, min_size=1, max_size=5
                 )
                 logger.info("Connection success")
+                return
             except Exception as e:
-                logger.error(f"Connection error: {e}")
+                logger.error(f"Connection attempt {attempt + 1} failed: {e}")
+                await asyncio.sleep(delay)
+        raise ConnectionError(f"Are not able to connect after {retries} retries")
 
     @classmethod
-    async def disconnect(cls):
+    async def disconnect(cls) -> None:
         if cls._pool:
             await cls._pool.close()
             cls._pool = None
@@ -26,12 +31,20 @@ class Database:
 
     @classmethod
     async def fetch(cls, query: str, *args):
-        async with cls._pool.acquire() as conn:
-            logger.info(f"Fetch: {query} | Params: {args}")
-            return await conn.fetch(query, *args)
+        try:
+            async with cls._pool.acquire() as conn:
+                logger.info(f"Fetch: {query} | Params: {args}")
+                return await conn.fetch(query, *args)
+        except Exception as e:
+            logger.error(f"Fetch query error: {e}")
+            raise
 
     @classmethod
     async def execute(cls, query: str, *args):
-        async with cls._pool.acquire() as conn:
-            logger.info(f"Running execution: {query} | Params: {args}")
-            return await conn.execute(query, *args)
+        try:
+            async with cls._pool.acquire() as conn:
+                logger.info(f"Running execution: {query} | Params: {args}")
+                return await conn.execute(query, *args)
+        except Exception as e:
+            logger.error(f"Fetch execute error: {e}")
+            raise
--- src/services/s3_uploader.py
+++ src/services/s3_uploader.py
@@ -2,22 +2,28 @@ import aioboto3
 import aiofiles
 
 from utils.config import config
+from utils.logger import logger
 
 
 class S3Uploader:
     @staticmethod
-    async def upload_to_s3(file_path, s3_key):
-        session = aioboto3.Session()
-        async with session.client(
+    async def upload_to_s3(file_path: str, s3_key: str):
+        try:
+            session = aioboto3.Session()
+            async with session.client(
                 "s3",
                 aws_access_key_id=config.AWS_ACCESS_KEY_ID,
-                aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
-        ) as s3_client:
-            async with aiofiles.open(file_path, "rb") as file:
-                await s3_client.put_object(
-                    Bucket=config.AWS_S3_BUCKET,
-                    Key=s3_key,
-                    Body=await file.read(),
-                    ContentType="application/json",
-                    ContentEncoding="gzip"
-                )
+                aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
+                region_name=config.AWS_REGION,
+            ) as s3_client:
+                async with aiofiles.open(file_path, "rb") as file:
+                    await s3_client.upload_fileobj(
+                        Bucket=config.AWS_S3_BUCKET,
+                        Key=s3_key,
+                        Body=await file.read(),
+                        ContentType="application/json",
+                        ContentEncoding="gzip",
+                    )
+                logger.info(f"File {file_path} uploaded (key: {s3_key})")
+        except Exception as e:
+            logger.error(f"Error in uploading file {file_path} to S3: {e}")
